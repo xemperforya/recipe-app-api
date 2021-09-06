7f98d697b82d030572e25ae049dab90aa4a86f07
Shantanu <shantanu@simplifyvms.com>
2021-09-06 14:01:57 +0530

    added create tag feature

--- app/core/models.py
+++ app/core/models.py
@@ -47,7 +47,7 @@ class Tag(models.Model):
     name = models.CharField(max_length=255)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
     )
 
 
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -6,7 +6,7 @@ from rest_framework.permissions import IsAuthenticated
 from core.models import Tag
 from recipe import serializers
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
     """Manage tags in the db"""
     authentication_classes = (TokenAuthentication, )
     permission_classes = (IsAuthenticated, )
@@ -18,3 +18,9 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
             return objects for the current authenticated user only
         """
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serliazer):
+        """
+            create new tag
+        """
+        serliazer.save(user=self.request.user)
